9e4d5512678bf1b24c9b437e476e4a10e5da03ca
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-03-24 12:55:56 +0100

    VkDevice

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,26 @@ fn main() -> Result<()> {
         unsafe { entry.create_instance(&create_info, None) }?
     };
 
-    let _physical_device = unsafe { instance.enumerate_physical_devices() }?
+    let physical_device = unsafe { instance.enumerate_physical_devices() }?
         .into_iter()
         .next()
         .context("No physical devices")?;
+
+    let device = {
+        let queue_priorities = [1.0];
+
+        let queue_create_infos = [
+            vk::DeviceQueueCreateInfo::builder()
+                .queue_family_index(0)
+                .queue_priorities(&queue_priorities)
+                .build()
+        ];
+
+        let create_info = vk::DeviceCreateInfo::builder()
+            .queue_create_infos(&queue_create_infos);
+
+        unsafe { instance.create_device(physical_device, &create_info, None) }?
+    };
 
     let event_loop = EventLoop::new()?;
 
@@ -39,6 +55,7 @@ fn main() -> Result<()> {
         }
     })?;
 
+    unsafe {   device.destroy_device(None);   }
     unsafe { instance.destroy_instance(None); }
 
     Ok(())
