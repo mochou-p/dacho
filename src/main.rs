f534d15dee2d73201184b9316320fa53c3ad32d5
mochou-p <vrecusko20@gmail.com>
2024-03-24 02:43:35 +0100

    window base

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 // dacho/src/main.rs
 
-fn main() {
-    println!("Hello, world!");
+use winit::{
+    event_loop::EventLoop,
+    window::WindowBuilder
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = EventLoop::new()?;
+
+    let _window = WindowBuilder::new()
+        .with_title("dacho")
+        .build(&event_loop)?;
+
+    event_loop.run(move |event, _| {
+        match event {
+            _ => ()
+        }
+    })?;
+
+    Ok(())
 }
 
