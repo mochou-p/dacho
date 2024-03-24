bf88d581ede3bbd26c44c5c43e296c2b9a6bd628
mochou-p <vrecusko20@gmail.com>
2024-03-24 12:42:49 +0100

    VkInstance

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 // dacho/src/main.rs
 
+use ash::vk;
+
 use winit::{
     event_loop::EventLoop,
     window::WindowBuilder
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let entry = unsafe { ash::Entry::load() }?;
+
+    let instance = {
+        let application_info = vk::ApplicationInfo::builder()
+            .api_version(vk::API_VERSION_1_3);
+
+        let create_info = vk::InstanceCreateInfo::builder()
+            .application_info(&application_info);
+
+        unsafe { entry.create_instance(&create_info, None) }?
+    };
+
     let event_loop = EventLoop::new()?;
 
     let _window = WindowBuilder::new()
@@ -17,6 +31,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             _ => ()
         }
     })?;
+
+    unsafe { instance.destroy_instance(None); }
 
     Ok(())
 }
