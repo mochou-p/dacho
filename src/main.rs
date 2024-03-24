70d025a791774481302ffb5fe823ffa6beef3f18
mochou-p <vrecusko20@gmail.com>
2024-03-24 17:01:12 +0100

    VkSurfaceKHR

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 
 use anyhow::{Context, Result};
 
-use ash::vk;
+use ash::{
+    extensions::khr::Surface,
+    vk
+};
+
+use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
 
 use winit::{
     event_loop::EventLoop,
@@ -10,14 +15,21 @@ use winit::{
 };
 
 fn main() -> Result<()> {
+    let event_loop = EventLoop::new()?;
+
     let entry = unsafe { ash::Entry::load() }?;
 
     let instance = {
         let application_info = vk::ApplicationInfo::builder()
             .api_version(vk::API_VERSION_1_3);
 
+        let extension_names = ash_window::enumerate_required_extensions(
+            event_loop.raw_display_handle()
+        )?;
+
         let create_info = vk::InstanceCreateInfo::builder()
-            .application_info(&application_info);
+            .application_info(&application_info)
+            .enabled_extension_names(extension_names);
 
         unsafe { entry.create_instance(&create_info, None) }?
     };
@@ -45,11 +57,19 @@ fn main() -> Result<()> {
 
     let _queue = unsafe { device.get_device_queue(0, 0) };
 
-    let event_loop = EventLoop::new()?;
-
-    let _window = WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("dacho")
         .build(&event_loop)?;
+
+    let surface_khr = unsafe { ash_window::create_surface(
+        &entry,
+        &instance,
+        window.raw_display_handle(),
+        window.raw_window_handle(),
+        None
+    ) }?;
+
+    let surface = Surface::new(&entry, &instance);
 
     event_loop.run(move |event, _| {
         match event {
@@ -57,8 +77,9 @@ fn main() -> Result<()> {
         }
     })?;
 
-    unsafe {   device.destroy_device(None);   }
-    unsafe { instance.destroy_instance(None); }
+    unsafe {  surface.destroy_surface(surface_khr, None); };
+    unsafe {   device.destroy_device(None);               };
+    unsafe { instance.destroy_instance(None);             };
 
     Ok(())
 }
