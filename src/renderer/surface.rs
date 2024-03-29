f3dc5006703343fe91ccddeb30a2d6fa1f57f70e
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-03-29 21:01:00 +0100

    struct Surface

---
+++ src/renderer/surface.rs
@@ -0,0 +1,49 @@
+// dacho/src/renderer/surface.rs
+
+use anyhow::Result;
+
+use ash::{
+    extensions::khr,
+    vk
+};
+
+use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
+
+use winit::window::Window;
+
+pub struct Surface {
+    pub loader:  khr::Surface,
+    pub surface: vk::SurfaceKHR
+}
+
+impl Surface {
+    pub fn new(
+        entry:    &ash::Entry,
+        instance: &ash::Instance,
+        window:   &Window
+    ) -> Result<Self> {
+        let loader = khr::Surface::new(&entry, &instance);
+
+        let surface = unsafe {
+            ash_window::create_surface(
+                &entry,
+                &instance,
+                window.raw_display_handle(),
+                window.raw_window_handle(),
+                None
+            )
+        }?;
+
+        Ok(
+            Self {
+                loader,
+                surface
+            }
+        )
+    }
+
+    pub fn destroy(&self) {
+        unsafe { self.loader.destroy_surface(self.surface, None); }
+    }
+}
+
