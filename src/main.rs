1bfc2cd04d1e707b22ddb219ddd4782a676b7c4f
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-04-12 13:47:46 +0200

    clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,7 @@
 
 use anyhow::Result;
 
-use winit::{
-    event::{DeviceEvent, Event, WindowEvent},
-    event_loop::EventLoop,
-    keyboard::{KeyCode::Escape, PhysicalKey::Code}
-};
+use winit::event_loop::EventLoop;
 
 use dacho::application::Application;
 
@@ -16,30 +12,7 @@ fn main() -> Result<()> {
     let mut application = Application::new(&event_loop)?;
 
     event_loop.run(move |event, elwt| {
-        match event {
-            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
-                elwt.exit();
-            },
-            Event::WindowEvent { event: WindowEvent::KeyboardInput { event, is_synthetic: false, .. }, .. } => {
-                if event.physical_key == Code(Escape) {
-                    elwt.exit();
-                }
-
-                application.keyboard_input(&event);
-            },
-            Event::DeviceEvent { event: DeviceEvent::MouseMotion { delta }, .. } => {
-                application.mouse_input(&delta);
-            },
-            Event::AboutToWait => {
-                application.renderer.wait_for_device();
-                application.update();
-                application.renderer.request_redraw();
-            },
-            Event::WindowEvent { event: WindowEvent::RedrawRequested, .. } => {
-                application.redraw();
-            },
-            _ => ()
-        }
+        application.handle_event(&event, elwt);
     })?;
 
     Ok(())
