fff8f2bd8e2e22eaa1f5d9d99d4b8cba7d857829
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-04-20 19:41:50 +0200

    clean up demo scene

--- src/application/scene.rs
+++ src/application/scene.rs
@@ -10,10 +10,14 @@ pub struct Scene;
 
 impl Scene {
     pub fn demo() -> Result<Vec<GeometryData>> {
+        let grid_len             = 3;
+        let grid_size            = 16;
+        let grass_per_chunk_axis = 32;
+
         let scene = vec![
             Self::demo_skybox()?,
-            Self::demo_tiles()?,
-            Self::demo_grass()?,
+            Self::demo_tiles(grid_len, grid_size)?,
+            Self::demo_grass(grid_len, grid_size, grass_per_chunk_axis)?,
             Self::demo_vignette()?
         ];
 
@@ -59,8 +63,11 @@ impl Scene {
         Ok(geometry_data)
     }
 
-    fn demo_tiles() -> Result<GeometryData> {
-        let grid_size = 16.0;
+    fn demo_tiles(
+        grid_len:  u8,
+        grid_size: u8
+    ) -> Result<GeometryData> {
+        let grid_size = grid_size as f32;
         let grid_half = grid_size * 0.5;
         let step_frac = 1.0 / grid_size;
 
@@ -78,11 +85,10 @@ impl Scene {
 
         let mut instances: Vec<f32> = vec![];
 
-        let i      = 15;
-        let offset = (i - 1) as f32 * 0.5;
+        let offset = (grid_len - 1) as f32 * 0.5;
 
-        for z in 0..i {
-            for x in 0..i {
+        for z in 0..grid_len {
+            for x in 0..grid_len {
                 instances.push(grid_size * (x as f32 - offset));
                 instances.push(0.0);
                 instances.push(grid_size * (z as f32 - offset));
@@ -103,7 +109,11 @@ impl Scene {
         Ok(geometry_data)
     }
 
-    fn demo_grass() -> Result<GeometryData> {
+    fn demo_grass(
+        grid_len:             u8,
+        grid_size:            u8,
+        grass_per_chunk_axis: u8
+    ) -> Result<GeometryData> {
         let vertices: Vec<f32> = vec![
              0.00, 4.0, 0.0,
              0.08, 1.8, 0.0,
@@ -120,24 +130,22 @@ impl Scene {
 
         let mut instances: Vec<f32> = vec![];
 
-        let grid_i    = (15 - 1) / 2;
-        let grid_size = 16.0;
-        let i         = 32;
-        let offset1   = grid_size / i as f32;
-        let offset2   = (i - 1) as f32 * 0.5;
+        let grid_i    = ((grid_len - 1) / 2) as i16;
+        let offset1   = grid_size as f32 / grass_per_chunk_axis as f32;
+        let offset2   = (grass_per_chunk_axis - 1) as f32 * 0.5;
 
         for chunk_z in -grid_i..grid_i + 1 {
             for chunk_x in -grid_i..grid_i + 1 {
-                for z in 0..i {
-                    for x in 0..i {
+                for z in 0..grass_per_chunk_axis {
+                    for x in 0..grass_per_chunk_axis {
                         let x_   = x as f32 - offset2;
                         let z_   = z as f32 - offset2;
 
                         let x__  = offset1 * x_ + noise(x_, z_ + chunk_x as f32) * offset1 * 0.5;
                         let z__  = offset1 * z_ + noise(z_, x_ + chunk_z as f32) * offset1 * 0.5;
 
-                        let x___ = chunk_x as f32 * grid_size;
-                        let z___ = chunk_z as f32 * grid_size;
+                        let x___ = chunk_x as f32 * grid_size as f32;
+                        let z___ = chunk_z as f32 * grid_size as f32;
 
                         instances.push(x__ + x___);
                         instances.push(0.0);
