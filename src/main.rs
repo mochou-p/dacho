5283d17d5336ab4882f9d0c000b333d7070c79c1
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-04-22 17:47:00 +0200

    struct Timer

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,26 @@
 
 use anyhow::Result;
 
-use winit::event_loop::EventLoop;
-
 #[cfg(debug_assertions)]
 use dacho::application::logger::Logger;
 
 use dacho::application::Application;
 
 fn main() -> Result<()> {
-    let event_loop = EventLoop::new()?;
+    #[cfg(debug_assertions)]
+    {
+        println!();
+        Logger::info("Creating EventLoop");
+    }
 
+    let     event_loop  = winit::event_loop::EventLoop::new()?;
     let mut application = Application::new(&event_loop)?;
 
     #[cfg(debug_assertions)]
     {
         println!();
         Logger::info("Running EventLoop");
+        Logger::indent(1);
     }
 
     event_loop.run(move |event, elwt| {
