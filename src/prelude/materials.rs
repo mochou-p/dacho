63c056ae70f37a98ec2757914184faec85f2a8a9
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-05-28 20:24:07 +0200

    add predefined materials

---
+++ src/prelude/materials.rs
@@ -0,0 +1,12 @@
+// dacho/src/prelude/materials.rs
+
+use super::types::V2;
+
+pub struct Material;
+
+#[allow(dead_code)]
+impl Material {
+    pub const ROUGH: V2 = V2::new(0.0, 1.0);
+    pub const METAL: V2 = V2::new(0.9, 0.1);
+}
+
