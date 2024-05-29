a54845f1d08338acce1a295a7831db48a909586f
mochou-p <vrecusko20@gmail.com>
2024-05-29 17:00:04 +0200

    add more defaults and helpers to prelude

--- src/prelude/materials.rs
+++ src/prelude/materials.rs
@@ -6,6 +6,11 @@ pub struct Material;
 
 impl Material {
     pub const ROUGH: V2 = V2::new(0.05, 0.95);
+    pub const MIXED: V2 = V2::new(0.40, 0.60);
     pub const METAL: V2 = V2::new(0.88, 0.11);
-}
 
+    #[allow(clippy::should_implement_trait)]
+    pub fn default() -> V2 {
+        Self::MIXED
+    }
+}
