c56f85fa51727384cfb1302090ac56e278c1b32e
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-07-14 16:59:09 +0200

    Entity removal

--- src/ecs/entity.rs
+++ src/ecs/entity.rs
@@ -5,16 +5,16 @@ use super::component::Component;
 
 #[derive(Debug)]
 pub struct Entity {
-    pub id:            u64,
-    pub parent:        Option<u64>,
-    pub children_ids:  Vec<u64>,
-    pub component_ids: Vec<u64>
+    pub id:             u64,
+    pub parent:         Option<u64>,
+    pub children_ids:   Vec<u64>,
+    pub components_ids: Vec<u64>
 }
 
 impl Entity {
     #[must_use]
     pub fn new(id: u64) -> Self {
-        Self { id, parent: None, children_ids: vec![], component_ids: vec![] }
+        Self { id, parent: None, children_ids: vec![], components_ids: vec![] }
     }
 }
 
