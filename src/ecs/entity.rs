014b3a6fa56e522b1a7231c5b46b11d269a12b0e
mochou-p <vrecusko20@gmail.com>
2024-07-15 04:04:21 +0200

    make ECS safe with pattern matching and Options

--- src/ecs/entity.rs
+++ src/ecs/entity.rs
@@ -5,16 +5,21 @@ use super::component::Component;
 
 #[derive(Debug)]
 pub struct Entity {
-    pub id:             u64,
-    pub parent:         Option<u64>,
-    pub children_ids:   Vec<u64>,
-    pub components_ids: Vec<u64>
+    pub id:               u64,
+    pub parent_id_option: Option<u64>,
+    pub children_ids:     Vec<u64>,
+    pub components_ids:   Vec<u64>
 }
 
 impl Entity {
     #[must_use]
     pub fn new(id: u64) -> Self {
-        Self { id, parent: None, children_ids: vec![], components_ids: vec![] }
+        Self { id, parent_id_option: None, children_ids: vec![], components_ids: vec![] }
+    }
+
+    #[must_use]
+    pub fn new_child(id: u64, parent_id: u64) -> Self {
+        Self { id, parent_id_option: Some(parent_id), children_ids: vec![], components_ids: vec![] }
     }
 }
 
