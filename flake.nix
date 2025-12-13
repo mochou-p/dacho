de37b940bfc6963409b6ddece57f7f85f86cbc51
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-12-14 01:49:58 +0900

    it makes more sense this way trust me

--- flake.nix
+++ flake.nix
@@ -1,5 +1,14 @@
 {
-  description = "nightly rust, x11/wayland, vulkan, glslang";
+  description = ''
+    dacho - rust vulkan game engine
+
+    1. building/running -> `nix develop` (default shell)
+    - for building: nightly rust & clippy, x11/wayland
+    - for running:  glslang, xkb, vulkan
+
+    2. debugging -> `nix develop .#debug`
+    - inherits default shell + vkconfig-gui
+  '';
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,10 +21,8 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs     =    import nixpkgs { inherit system overlays; };
-      in
-      {
-        devShells.default = with pkgs; mkShell {
-          # build machine
+
+        defaultShell = with pkgs; {
           nativeBuildInputs = [
             openssl
             pkg-config
@@ -24,29 +31,50 @@
                 extensions = [ "clippy-preview" ];
               })
             )
-          ];
 
-          # host machine
-          buildInputs = [
-            glslang                  # shader compiler
-            libxkbcommon             # keyboard
-            vulkan-loader
-            vulkan-tools-lunarg      # vkconfig (dynamic validation layers)
-            vulkan-validation-layers # for vkconfig
             wayland
           ];
 
-          # environment
+          buildInputs = [
+            glslang
+          ];
+
           shellHook = ''
             export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath [
               libxkbcommon
               vulkan-loader
-              vulkan-validation-layers
-              wayland
             ] }"
-
-            export VK_LAYER_PATH="$VK_LAYER_PATH:${vulkan-validation-layers}/share/vulkan/explicit_layer.d"
           '';
+        };
+      in
+      {
+        devShells = with pkgs; {
+          default = mkShell (defaultShell // {
+            shellHook = defaultShell.shellHook + ''
+              echo
+              echo -e "\x1b[42;30;1m                     default shell                     \x1b[0m"
+              echo -e "\x1b[40;32m build project   \x1b[2m:\x1b[22;92;1m cargo build                         \x1b[0m"
+              echo -e "\x1b[40;32m compile shaders \x1b[2m:\x1b[22;92;1m ./examples/usage/compile_shaders.sh \x1b[0m"
+              echo -e "\x1b[40;32m run example     \x1b[2m:\x1b[22;92;1m ./target/debug/usage                \x1b[0m"
+              echo
+            '';
+          });
+
+          debug = mkShell (defaultShell // {
+            nativeBuildInputs = defaultShell.nativeBuildInputs ++ [
+              vulkan-tools-lunarg
+            ];
+
+            shellHook = defaultShell.shellHook + ''
+              echo
+              echo -e "\x1b[43;30;1m                      debug shell                      \x1b[0m"
+              echo -e "\x1b[40;33m build project   \x1b[2m:\x1b[22;93;1m cargo build                         \x1b[0m"
+              echo -e "\x1b[40;33m compile shaders \x1b[2m:\x1b[22;93;1m ./examples/usage/compile_shaders.sh \x1b[0m"
+              echo -e "\x1b[40;33m debug vulkan    \x1b[2m:\x1b[22;93;1m vkconfig-gui &                      \x1b[0m"
+              echo -e "\x1b[40;33m run example     \x1b[2m:\x1b[22;93;1m ./target/debug/usage                \x1b[0m"
+              echo
+            '';
+          });
         };
       }
     );
